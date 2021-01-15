61f5e24002b3feffc16b3c64d60e90c17ee511be
Josh Jacobson <joshjacobson14@gmail.com>
2021-01-14 18:28:40 -0800

    lecture work-in-progress

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
-    axios.get("http://localhost:5000/api/data", { headers: { Authorization: token } })
+    return axios.create({
+        baseUrl: "http://localhost:5000/api",
+        headers: { Authorization: token }
+    });
 }
