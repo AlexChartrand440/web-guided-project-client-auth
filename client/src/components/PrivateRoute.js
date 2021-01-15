abadc5259c49a7e9aad53a2f5c0a7a5d3f3ad232
Josh Jacobson <joshjacobson14@gmail.com>
2021-01-14 17:52:51 -0800

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -14,11 +14,13 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       render={(componentProps) => {
         // if the token is in localStorage, render the component
         if (localStorage.getItem("token")) {
+          console.log("token found, rendering component");
           return <Component {...componentProps} />
         }
 
         // Otherwise, redirect to login
         else {
+          console.log("no token found, redirect to login");
           return <Redirect to="/login" />
         }
       }}
