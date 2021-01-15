46d64f8fb66a1320699165fba965fbe01b6dc735
Josh Jacobson <joshjacobson14@gmail.com>
2021-01-14 17:26:31 -0800

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 
 // Private route requirements:
 // 1. It has the same API (interface) as <Route/>
-// 2. If an auth token exists, it renders a <Route/> and passes all props through to it
+// 2. If an auth token exists, it renders a <Route/> and passes all props through to the component
 // 3. Otherwise, redirect to the login page
 
 // take the component prop, rename it Component (capitalized so that it'll render in JSX) and assign the rest of the props to a variable called "rest"
